35dcb4d7191d243222125dbc0b7c3df66c850ad2
agjwong <adamgj.wong@gmail.com>
2016-01-15 16:54:42 -0500

    All good except for final post to save files

--- create_gist.py
+++ create_gist.py
@@ -1,6 +1,8 @@
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
 import requests
+import iampython
+import json
 
 class AuthorizeHandler(IPythonHandler):
     def get(self):
@@ -8,46 +10,60 @@ class AuthorizeHandler(IPythonHandler):
 
         args = self.request.arguments
        
-        access_code = args["code"]
+        access_code = args["code"][0]
+        access_code = access_code.decode('ascii')
+        
 
-        req = requests.post("https://github.com/login/oauth/access_token", 
+        response = requests.post("https://github.com/login/oauth/access_token", 
             data = {"client_id":"bd8256a4bcc8042c8152", 
-            "client_secret" : "5a80adf6f71a9c894345102fa74b6fad90fbdc54",
+            "client_secret" : "1caf8f68e15e01e49fe52b3259c18cbc714d0438",
             "code" : access_code,
-            "redirect_uri" : "http://localhost:8888/save_gist"})
+            "redirect_uri" : "http://localhost:8888/save_gist"},
+            headers = {"Accept" : "application/json"})
 
-
-"""class SaveGistHandler(IPythonHandler):
-    def get(self):
         print("Saving gist. . .")
+        args = json.loads(response.text)
+        print(args)
 
-        args = self.request.arguments
-       
+        # change these to .get to prevent exceptions
         access_token = args["access_token"]
         token_type = args["token_type"]
         scope = args["scope"]
 
         tokenDict = { "Authorization" : "token " + access_token }
 
+        print(tokenDict)
         pyFiles = {
             'description' : 'My python file',
-            'public' : 'true',
             'files' : {
                 "a.txt" : {
                     "content" : "I am a python file"
                     },
                 "b.txt" : {
                     "content" : "I am also a python file"
+                    }
                 }
         }
-
+        print("Saving gist. . .")
         # TODO : Validate the token
-        req = requests.get("https://api.github.com/gists", 
-            data = pyFiles,
-            headers = tokenDict)
+        response = requests.post("https://api.github.com/gists", 
+            data = {
+            'description' : 'My python file',
+            'files' : {
+                "a.txt" : {
+                    "content" : "I am a python file"
+                    },
+                "b.txt" : {
+                    "content" : "I am also a python file"
+                    }
+                }
+            },
+            headers = { "Authorization" : "token " + access_token })
+
+        print(response.content)
 
 
-        print("Saving gist. . .")* """
+        print("Saving gist. . .")
 
 
 def load_jupyter_server_extension(nb_server_app):
@@ -61,6 +77,3 @@ def load_jupyter_server_extension(nb_server_app):
     host_pattern = '.*$'
     route_pattern1 = url_path_join(web_app.settings['base_url'], '/authorize')
     web_app.add_handlers(host_pattern, [(route_pattern1, AuthorizeHandler)])
-    print("I am here")
-    route_pattern2 = url_path_join(web_app.settings['base_url'], '/save_gist')
-    web_app.add_handlers(host_pattern, [(route_pattern2, SaveGistHandler)])
