c1756592fc8bcee1348d20ad8312a536aaf1589f
Michael Droettboom <mdboom@gmail.com>
2018-02-05 11:18:53 -0500

    Fix #53, Fix #56: Don't corrupt notebook.json when no oauth_client_id
    When there is no `c.NotebookGist.oauth_client_id` in jupyter_notebook_config.py,
    `self.config.NotebookGist.oauth_client_id` is a lazy value, which can't be
    serialized to JSON.  This crashes the writing of `notebook.json` by Jupyter,
    and corrupts the `notebook.json` file.  This fix makes sure that value is a
    string, otherwise it just sets it to `None`.  When `None`, the frontend will
    helpfully warn (as it always has) that the user hasn't set up their oauth ids.

--- src/jupyter_notebook_gist/config.py
+++ src/jupyter_notebook_gist/config.py
@@ -19,6 +19,9 @@ class NotebookGist(LoggingConfigurable):
         super(NotebookGist, self).__init__(*args, **kwargs)
         # update the frontend settings with the currently passed
         # OAUTH client id
+        client_id = self.config.NotebookGist.oauth_client_id
+        if not isinstance(client_id, (str, bytes)):
+            client_id = None
         self.config_manager.update('notebook', {
-            'oauth_client_id': self.config.NotebookGist.oauth_client_id,
+            'oauth_client_id': client_id,
         })
